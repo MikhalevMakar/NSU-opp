1011bc169744543873a965b3423c97050cf8bdd9
MikhalevMakar <mikhalevmakar@mail.ru>
2023-03-04 11:02:40 +0700

    add something parrallel pragma

---
+++ Task2/Version2/cmake-build-release-remote-host-1/CMakeFiles/Version2.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Version2.dir/main.cpp.o"
+  "Version2"
+  "Version2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Version2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
